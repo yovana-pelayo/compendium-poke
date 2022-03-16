afd028d7dfd45b2a288dbb4f9eb6850d6ee9fa0e
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-03-16 16:39:31 -0700

    pokemon names are appearing on broswer. Sized down header

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,4 +1,24 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchPokemon } from '../services/pokemon';
+import './Main.css';
 
-export default function Main() {}
+export default function Main() {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchPokemon();
+      setPokemon(data);
+    };
+    fetchData();
+  }, []);
+  return (
+    <div className="pokemon-name">
+      {pokemon.map((poke) => (
+        <div key={poke.id}>
+          <p>{poke.pokemon}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
