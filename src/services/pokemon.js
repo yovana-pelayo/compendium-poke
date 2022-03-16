42e0dc7cfde34b4a8a18bc2e79b4b5e4e1669912
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-03-16 15:40:38 -0700

    created fetchPokemon function

--- src/services/pokemon.js
+++ src/services/pokemon.js
@@ -1,3 +1,5 @@
 export async function fetchPokemon() {
   const resp = await fetch(' https://pokedex-alchemy.herokuapp.com/api/pokedex');
+  const data = await resp.jason();
+  return data.results;
 }
