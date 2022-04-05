2311acc89d4437d48f23ccb9111de7febbbe8556
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-04-05 08:48:02 -0700

    drop down menu shoes all pokemon. next step is to create pagnation and filter pokemon by type

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import PokeCard from '../Components/PokeCard';
-import { fetchPokemon, fetchFilteredPokemon } from '../services/pokemon';
+import TypeDropdown from '../Components/TypeDropdown';
+import { fetchFilteredPokemon, fetchTypes } from '../services/pokemon';
 import './Main.css';
 
 export default function Main() {
@@ -8,34 +9,36 @@ export default function Main() {
   const [loading, setLoading] = useState(true);
   const [types, setTypes] = useState([]);
   // const [search, setSearch] = useState('');
+  const [selectedType, setSelectedType] = useState('');
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await fetchPokemon();
-      setPokemon(data);
-      setLoading(false);
-    };
-    fetchData();
-  }, []);
-  useEffect(() => {
-    const fetchData = async () => {
-      const typesData = await fetchTypes();
+      const typesData = await fetchTypes(selectedType, setSelectedType);
       setTypes(['all', ...typesData]);
     };
     fetchData();
   }, []);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const typesData = await fetchTypes();
+  //     setTypes(['all', ...typesData]);
+  //   };
+  //   fetchData();
+  // }, []);
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await fetchFilteredPokemon();
+      const data = await fetchFilteredPokemon(selectedType, page);
       setPokemon(data);
       setLoading();
     };
     fetchData();
-  }, []);
+  }, [selectedType, page]);
   if (loading) return <div className="o-pokeball">icon</div>;
   return (
     <div>
+      <TypeDropdown types={types} setSelectedType={setSelectedType} selectedType={selectedType} />
       {pokemon.map((poke) => (
         <PokeCard key={poke.id} {...poke} />
       ))}
